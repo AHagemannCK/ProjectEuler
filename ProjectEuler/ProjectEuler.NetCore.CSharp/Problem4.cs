db8382752158cd32d1f1e52d5409cee4b3494152
Alexander Hagemann <alexander.hagemann@cloudkasten.net>
2021-01-17 23:39:05 +0100

    Implemented solution for problem 4

---
+++ ProjectEuler/ProjectEuler.NetCore.CSharp/Problem4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler.NetCore.CSharp
+{
+    public static class Problem4
+    {
+        public static void Solve()
+        {
+            var allPalindromes = new List<string>();
+            for (int firstNumberToCheck = 999; firstNumberToCheck >= 100; firstNumberToCheck--)
+            {
+                for (int numberToCreateProductWith = 999; numberToCreateProductWith >= 100; numberToCreateProductWith--)
+                {
+                    var product = firstNumberToCheck * numberToCreateProductWith;
+                    if (IsPalindrome(product.ToString()))
+                        allPalindromes.Add(product.ToString());
+                }
+            }
+            Console.WriteLine($"Problem 4 solution: {allPalindromes.Select(x => Convert.ToInt32(x)).OrderByDescending(x => x).First()}");
+        }
+
+        private static bool IsPalindrome(string stringToCheck)
+        {
+            var forward = stringToCheck.ToCharArray();
+            return forward.SequenceEqual(forward.Reverse());
+        }
+    }
+}
